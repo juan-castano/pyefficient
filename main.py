ce56056da25c51a4282ddce1af466e4d725e22c8
Juan Castaño <jucacase92@gmail.com>
2014-12-07 00:22:45 -0500

    Analizador con reglas basicas

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from analizador import Analizador
+
+
+def main():
+    analizador = Analizador()
+    analizador.test()
+
+if __name__ == '__main__':
+    main()
